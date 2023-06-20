426c4b0d8013f18a205d104c741c58698e1d24c4
murremway <76540706+murremway@users.noreply.github.com>
2023-06-20 15:50:25 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,8 @@
 provider "oci" {
-  tenancy_ocid     = "ocid1.tenancy.oc1..aaaaaaaanmhxwh7dwzgzvbqmllrsge66t3v4hm4bd3ngbxmaw6bq5zyxydxa"
-  user_ocid        = "ocid1.user.oc1..aaaaaaaakt26t3tvlazkdibzzp66bhlvt7ucbdghvhdc2majw2fwqpzsq2la"
-  fingerprint      = "ae:90:ab:79:a5:66:d2:4e:b2:7f:36:20:6f:05:67:71"
-  private_key_path = "C:/Users/murre/.oci/challenge"
+  tenancy_ocid     = "var.compartment_id"
+  user_ocid        = "var.user_ocid"
+  fingerprint      = "fingerprint"
+  private_key_path = "var.private_key_path"
   region           = "us-ashburn-1"
 }
 
