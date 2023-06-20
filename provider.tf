4ea52a01444245f0cde4d38fe8ece21399aef5fa
murremway <76540706+murremway@users.noreply.github.com>
2023-06-20 15:50:52 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 provider "oci" {
   tenancy_ocid     = "var.compartment_id"
   user_ocid        = "var.user_ocid"
-  fingerprint      = "fingerprint"
+  fingerprint      = "var.fingerprint"
   private_key_path = "var.private_key_path"
   region           = "us-ashburn-1"
 }
