a119ad0c7175299897113bf4b1feb5d68f76848d
murremway <76540706+murremway@users.noreply.github.com>
2023-06-20 15:40:06 -0400

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,18 @@
+variable "tenancy_ocid" {
+  default = "TENANCY OCID"
+}
+variable "user_ocid" {
+  default = "USER OCID"
+}
+variable "fingerprint" {
+  default = "FINGERPRINT"
+}
+variable "private_key_path" {
+  default = "PATH TO PRIVATE KEY"
+}
+variable "region" {
+  default = "us-ashburn-1"
+}
+variable "compartment_id" {
+  default = "COMPARTMENT OCID"
+}
